b534b714e89210c6cc677777de5ea980f3cd82db
Keith Flanagan <k.s.flanagan@gmail.com>
2016-05-04 16:10:17 +0100

    Support for an external volume for dynamically mounting the hub config, and MongoDB data files. If non-existent, these will be created on first boot.

--- Dockerfile
+++ Dockerfile
@@ -19,6 +19,14 @@ RUN cd /opt && git clone https://github.com/ICO2S/synbiohub.git --depth 1 --bran
 RUN cd /opt/synbiohub && npm install && npm install -g forever
 RUN chown -R ubuntu:ubuntu /opt/synbiohub
 
+# Store the synbiohub config in a different directory so that we can mount it in a Docker volume
+# Also store the MongoDB data directory externally to the container image
+RUN mkdir /mnt/data \
+	&& mv /opt/synbiohub/config.json /opt/synbiohub/default-config.json \
+	&& ln -s /mnt/data/hub-config/config.json /opt/synbiohub/config.json \
+	&& rm -r /var/lib/mongodb \
+	&& ln -s /mnt/data/mongodb /var/lib/mongodb
+
 
 # Here's a custom startup script that will run on container bootup
 COPY startup.sh /
--- startup.sh
+++ startup.sh
@@ -1,5 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-cd /opt/synbiohub/ && su ubuntu -c "forever synbiohub.js"
+# If the user mounts an incomplete or empty data directory (such as first startup), create some defaults:
+if ! [ -d "/mnt/data/hub-config" ] ; then
+  mkdir /mnt/data/hub-config
+fi
 
+if ! [ -e "/mnt/data/hub-config/config.json" ] ; then
+  cp /opt/synbiohub/default-config.json /mnt/data/hub-config/config.json
+fi
+
+if ! [ -d "/mnt/data/mongodb" ] ; then
+  mkdir /mnt/data/mongodb
+fi
+
+# Ensure files in the mounted volume have appropriate permissions
+chown -R ubuntu:ubuntu /mnt/data/hub-config
+chown -R mongodb:mongodb /mnt/data/mongodb
+
+
+# Starts the database
+/usr/sbin/service mongodb start
+sleep 10
+
+# Starts the hub once the database is up
+cd /opt/synbiohub/ && su ubuntu -c "forever synbiohub.js" &
+
+# A hack to keep the Docker image alive, since the above are forked
+tail -f /dev/null
 
